efc9b16470b8a04456924ac1cc59e51a59081967
rayct <rayct@icloud.com>
2022-08-21 13:52:28 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/responsive/responsive_layout.dart';
+// import 'package:whatsapp_clone/screens/mobile_layout_screen.dart';
+// import 'package:whatsapp_clone/screens/web_layout_screen.dart';
+// import 'package:whatsapp_ui/utils/responsive_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WhatsApp Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileLayoutScreen(),
+        webScreenLayout: WebLayoutScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/mobile_chat_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MobileChatScreen extends StatelessWidget {
+  const MobileChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('This is Mobile'),
+      ),
+  }
+}
