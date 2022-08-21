b7723ea679605f34abcee1fc5bed869326bf06bb
rayct <rayct@icloud.com>
2022-08-22 00:55:07 +0100

    Completed Landing Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:whatsapp_clone/firebase_options.dart';
-import 'package:whatsapp_clone/screens/mobile_layout_screen.dart';
-import 'package:whatsapp_clone/screens/web_layout_screen.dart';
-import 'package:whatsapp_clone/utils/responsive_layout.dart';
+// import 'package:whatsapp_clone/utils/responsive_layout.dart';
+// import 'package:whatsapp_clone/screens/web_layout_screen.dart';
+// import 'package:whatsapp_clone/screens/mobile_layout_screen.dart';
+import 'package:whatsapp_clone/features/landing/screens/landing_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,10 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
-      home: const ResponsiveLayout(
-        mobileScreenLayout: MobileLayoutScreen(),
-        webScreenLayout: WebLayoutScreen(),
-      ),
+      home: const LandingScreen(),
     );
   }
 }
