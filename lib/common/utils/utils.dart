ae1792d8827767c6cb0041d0843a47e27a3ee52d
rayct <rayct@icloud.com>
2022-08-24 01:48:03 +0100

    User Info Screen UI

--- lib/common/utils/utils.dart
+++ lib/common/utils/utils.dart
@@ -1,6 +1,6 @@
-// import 'dart:io';
+import 'dart:io';
 import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
+import 'package:image_picker/image_picker.dart';
 // import 'package:enough_giphy_flutter/enough_giphy_flutter.dart';
 
 void showSnackBar({required BuildContext context, required String content}) {
@@ -11,20 +11,20 @@ void showSnackBar({required BuildContext context, required String content}) {
   );
 }
 
-// Future<File?> pickImageFromGallery(BuildContext context) async {
-//   File? image;
-//   try {
-//     final pickedImage =
-//         await ImagePicker().pickImage(source: ImageSource.gallery);
+Future<File?> pickImageFromGallery(BuildContext context) async {
+  File? image;
+  try {
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
 
-//     if (pickedImage != null) {
-//       image = File(pickedImage.path);
-//     }
-//   } catch (e) {
-//     showSnackBar(context: context, content: e.toString());
-//   }
-//   return image;
-// }
+    if (pickedImage != null) {
+      image = File(pickedImage.path);
+    }
+  } catch (e) {
+    showSnackBar(context: context, content: e.toString());
+  }
+  return image;
+}
 
 // Future<File?> pickVideoFromGallery(BuildContext context) async {
 //   File? video;
