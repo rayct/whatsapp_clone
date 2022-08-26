82588e79884a2f3de68a50866f37d6c260777c0b
rayct <rayct@icloud.com>
2022-08-26 01:59:12 +0100

    Saving User Data To Firestore

--- lib/features/landing/screens/auth/controller/auth_controller.dart
+++ lib/features/landing/screens/auth/controller/auth_controller.dart
@@ -1,31 +1,31 @@
-// import 'dart:io';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/models/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone/features/landing/screens/auth/repository/auth_repository.dart';
-// import 'package:whatsapp_clone/models/user_model.dart';
 
 final authControllerProvider = Provider((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
-  return AuthController(authRepository: authRepository);
+  return AuthController(authRepository: authRepository, ref: ref);
 });
 
-// final userDataAuthProvider = FutureProvider((ref) {
-//   final authController = ref.watch(authControllerProvider);
-//   return AuthController(authRepository: authRepository);
-// });
+final userDataAuthProvider = FutureProvider((ref) {
+  final authController = ref.watch(authControllerProvider);
+  return authController.getUserData();
+});
 
 class AuthController {
   final AuthRepository authRepository;
-  // final ProviderRef ref;
+  final ProviderRef ref;
   AuthController({
     required this.authRepository,
-    // required this.ref,
+    required this.ref,
   });
 
-  // Future<UserModel?> getUserData() async {
-  //   UserModel? user = await authRepository.getCurrentUserData();
-  //   return user;
-  // }
+  Future<UserModel?> getUserData() async {
+    UserModel? user = await authRepository.getCurrentUserData();
+    return user;
+  }
 
   void signInWithPhone(BuildContext context, String phoneNumber) {
     authRepository.signInWithPhone(context, phoneNumber);
@@ -39,21 +39,21 @@ class AuthController {
     );
   }
 
-  // void saveUserDataToFirebase(
-  //     BuildContext context, String name, File? profilePic) {
-  //   authRepository.saveUserDataToFirebase(
-  //     name: name,
-  //     profilePic: profilePic,
-  //     ref: ref,
-  //     context: context,
-  //   );
-  // }
+  void saveUserDataToFirebase(
+      BuildContext context, String name, File? profilePic) {
+    authRepository.saveUserDataToFirebase(
+      name: name,
+      profilePic: profilePic,
+      ref: ref,
+      context: context,
+    );
+  }
 
-  // Stream<UserModel> userDataById(String userId) {
-  //   return authRepository.userData(userId);
-  // }
+  Stream<UserModel> userDataById(String userId) {
+    return authRepository.userData(userId);
+  }
 
-  // void setUserState(bool isOnline) {
-  //   authRepository.setUserState(isOnline);
-  // }
+  void setUserState(bool isOnline) {
+    authRepository.setUserState(isOnline);
+  }
 }
--- lib/features/landing/screens/auth/repository/auth_repository.dart
+++ lib/features/landing/screens/auth/repository/auth_repository.dart
@@ -1,19 +1,14 @@
-// import 'dart:io';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:whatsapp_clone/models/user_model.dart';
 import 'package:whatsapp_clone/common/utils/utils.dart';
-import 'package:whatsapp_clone/features/landing/screens/auth/screens/otp_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_clone/screens/mobile_layout_screen.dart';
+import 'package:whatsapp_clone/features/landing/screens/auth/screens/otp_screen.dart';
+import 'package:whatsapp_clone/common/repositories/common_firebase_storage_repository.dart';
 import 'package:whatsapp_clone/features/landing/screens/auth/screens/user_information_screen.dart';
-// import 'package:whatsapp_clone/common/repositories/common_firebase_storage_repository.dart';
-// import 'package:whatsapp_clone/common/utils/utils.dart';
-// import 'package:whatsapp_clone/features/auth/screens/otp_screen.dart';
-// import 'package:whatsapp_clone/features/auth/screens/user_information_screen.dart';
-// import 'package:whatsapp_clone/models/user_model.dart';
-// import 'package:whatsapp_clone/mobile_layout_screen.dart';
 
 final authRepositoryProvider = Provider(
   (ref) => AuthRepository(
@@ -30,16 +25,16 @@ class AuthRepository {
     required this.firestore,
   });
 
-  // Future<UserModel?> getCurrentUserData() async {
-  //   var userData =
-  //       await firestore.collection('users').doc(auth.currentUser?.uid).get();
+  Future<UserModel?> getCurrentUserData() async {
+    var userData =
+        await firestore.collection('users').doc(auth.currentUser?.uid).get();
 
-  //   UserModel? user;
-  //   if (userData.data() != null) {
-  //     user = UserModel.fromMap(userData.data()!);
-  //   }
-  //   return user;
-  // }
+    UserModel? user;
+    if (userData.data() != null) {
+      user = UserModel.fromMap(userData.data()!);
+    }
+    return user;
+  }
 
   void signInWithPhone(BuildContext context, String phoneNumber) async {
     try {
@@ -86,60 +81,60 @@ class AuthRepository {
     }
   }
 
-//   void saveUserDataToFirebase({
-//     required String name,
-//     required File? profilePic,
-//     required ProviderRef ref,
-//     required BuildContext context,
-//   }) async {
-//     try {
-//       String uid = auth.currentUser!.uid;
-//       String photoUrl =
-//           'https://png.pngitem.com/pimgs/s/649-6490124_katie-notopoulos-katienotopoulos-i-write-about-tech-round.png';
+  void saveUserDataToFirebase({
+    required String name,
+    required File? profilePic,
+    required ProviderRef ref,
+    required BuildContext context,
+  }) async {
+    try {
+      String uid = auth.currentUser!.uid;
+      String photoUrl =
+          'https://png.pngitem.com/pimgs/s/649-6490124_katie-notopoulos-katienotopoulos-i-write-about-tech-round.png';
 
-//       if (profilePic != null) {
-//         photoUrl = await ref
-//             .read(commonFirebaseStorageRepositoryProvider)
-//             .storeFileToFirebase(
-//               'profilePic/$uid',
-//               profilePic,
-//             );
-//       }
+      if (profilePic != null) {
+        photoUrl = await ref
+            .read(commonFirebaseStorageRepositoryProvider)
+            .storeFileToFirebase(
+              'profilePic/$uid',
+              profilePic,
+            );
+      }
 
-//       var user = UserModel(
-//         name: name,
-//         uid: uid,
-//         profilePic: photoUrl,
-//         isOnline: true,
-//         phoneNumber: auth.currentUser!.phoneNumber!,
-//         groupId: [],
-//       );
+      var user = UserModel(
+        name: name,
+        uid: uid,
+        profilePic: photoUrl,
+        isOnline: true,
+        phoneNumber: auth.currentUser!.phoneNumber!,
+        groupId: [],
+      );
 
-//       await firestore.collection('users').doc(uid).set(user.toMap());
+      await firestore.collection('users').doc(uid).set(user.toMap());
 
-//       Navigator.pushAndRemoveUntil(
-//         context,
-//         MaterialPageRoute(
-//           builder: (context) => const MobileLayoutScreen(),
-//         ),
-//         (route) => false,
-//       );
-//     } catch (e) {
-//       showSnackBar(context: context, content: e.toString());
-//     }
-//   }
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MobileLayoutScreen(),
+        ),
+        (route) => false,
+      );
+    } catch (e) {
+      showSnackBar(context: context, content: e.toString());
+    }
+  }
 
-//   Stream<UserModel> userData(String userId) {
-//     return firestore.collection('users').doc(userId).snapshots().map(
-//           (event) => UserModel.fromMap(
-//             event.data()!,
-//           ),
-//         );
-//   }
+  Stream<UserModel> userData(String userId) {
+    return firestore.collection('users').doc(userId).snapshots().map(
+          (event) => UserModel.fromMap(
+            event.data()!,
+          ),
+        );
+  }
 
-//   void setUserState(bool isOnline) async {
-//     await firestore.collection('users').doc(auth.currentUser!.uid).update({
-//       'isOnline': isOnline,
-//     });
-//   }
+  void setUserState(bool isOnline) async {
+    await firestore.collection('users').doc(auth.currentUser!.uid).update({
+      'isOnline': isOnline,
+    });
+  }
 }
