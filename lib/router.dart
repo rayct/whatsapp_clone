b56c4a30189378a8b08db72b49ea3d40c7b361cd
rayct <rayct@icloud.com>
2022-08-27 02:24:47 +0100

    Displaying Contacts in Users Phone

--- lib/router.dart
+++ lib/router.dart
@@ -5,6 +5,7 @@ import 'package:whatsapp_clone/common/widgets/error.dart';
 import 'package:whatsapp_clone/features/landing/screens/auth/screens/login_screen.dart';
 import 'package:whatsapp_clone/features/landing/screens/auth/screens/otp_screen.dart';
 import 'package:whatsapp_clone/features/landing/screens/auth/screens/user_information_screen.dart';
+import 'package:whatsapp_clone/features/select_contacts/screens/select_contacts_screen.dart';
 // import 'package:whatsapp_clone/features/auth/screens/user_information_screen.dart';
 // import 'package:whatsapp_clone/features/group/screens/create_group_screen.dart';
 // import 'package:whatsapp_clone/features/select_contacts/screens/select_contacts_screen.dart';
@@ -31,10 +32,10 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(
         builder: (context) => const UserInformationScreen(),
       );
-//     case SelectContactsScreen.routeName:
-//       return MaterialPageRoute(
-//         builder: (context) => const SelectContactsScreen(),
-//       );
+    case SelectContactsScreen.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const SelectContactsScreen(),
+      );
 //     case MobileChatScreen.routeName:
 //       final arguments = settings.arguments as Map<String, dynamic>;
 //       final name = arguments['name'];
