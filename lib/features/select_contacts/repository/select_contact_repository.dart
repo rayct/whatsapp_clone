9ce4da82c78323c1ada70f8e8bce2c766d8ea681
rayct <rayct@icloud.com>
2022-09-21 01:30:59 +0100

    Displaying Contacts in User’s Phone - Completed

--- lib/features/select_contacts/repository/select_contact_repository.dart
+++ lib/features/select_contacts/repository/select_contact_repository.dart
@@ -4,6 +4,7 @@ import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone/common/utils/utils.dart';
 import 'package:whatsapp_clone/models/user_model.dart';
+import 'package:whatsapp_clone/screens/mobile_chat_screen.dart';
 // import 'package:whatsapp_clone/models/user_model.dart';
 // import 'package:flutter_contacts/flutter_contacts.dart';
 // import 'package:whatsapp_clone/common/utils/utils.dart';
@@ -49,14 +50,7 @@ class SelectContactRepository {
         if (selectedPhoneNum == userData.phoneNumber) {
           isFound = true;
 
-//           // Navigator.pushNamed(
-//           //   context,
-//           //   MobileChatScreen.routeName,
-//           //   arguments: {
-//           //     'name': userData.name,
-//           //     'uid': userData.uid,
-//           //   },
-//           // );
+          Navigator.pushNamed(context, MobileChatScreen.routeName);
         }
       }
 
--- lib/router.dart
+++ lib/router.dart
@@ -7,6 +7,7 @@ import 'package:whatsapp_clone/features/auth/screens/login_screen.dart';
 import 'package:whatsapp_clone/features/auth/screens/otp_screen.dart';
 import 'package:whatsapp_clone/features/auth/screens/user_information_screen.dart';
 import 'package:whatsapp_clone/features/select_contacts/screens/select_contacts_screen.dart';
+import 'package:whatsapp_clone/screens/mobile_chat_screen.dart';
 // import 'package:whatsapp_clone/features/chat/screens/mobile_chat_screen.dart';
 // import 'package:whatsapp_clone/features/select_contacts/screens/select_contacts_screen.dart';
 
@@ -35,20 +36,20 @@ Route<dynamic> generateRoute(RouteSettings settings) {
         builder: (context) => const SelectContactsScreen(),
       );
 
-//     case MobileChatScreen.routeName:
+    case MobileChatScreen.routeName:
 //       final arguments = settings.arguments as Map<String, dynamic>;
 //       final name = arguments['name'];
 //       final uid = arguments['uid'];
 //       // final isGroupChat = arguments['isGroupChat'];
 //       // final profilePic = arguments['profilePic'];
-//       return MaterialPageRoute(
-//         builder: (context) => MobileChatScreen(
+      return MaterialPageRoute(
+        builder: (context) => MobileChatScreen(
 //           name: name,
 //           uid: uid,
 //           //     // isGroupChat: isGroupChat,
 //           //     // profilePic: profilePic,
-//         ),
-//       );
+            ),
+      );
 
 // // //     case ConfirmStatusScreen.routeName:
 // // //       final file = settings.arguments as File;
